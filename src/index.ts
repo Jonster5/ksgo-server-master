4f21e2e97eeb488fbde093820ad03ed47648e7d3
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2021-02-04 23:51:13 -0500

    user serverlist updates, and server connection support added

--- src/index.ts
+++ src/index.ts
@@ -1,120 +1,144 @@
 import WebSocket from 'ws';
-// import url from 'url';
-// import fs from 'fs';
 
 const port = process.env.PORT || 4000;
 const WSS = new WebSocket.Server({ port: parseInt(`${port}`) });
 
-const clients = new Map();
-const servers = new Map();
+type MessageID =
+	| 'server-connect'
+	| 'server-connect-confirm'
+	| 'user-connect'
+	| 'user-connect-confirm'
+	| 'user-serverlist-update';
+
+interface ClientListItem {
+	id: string;
+	name: string;
+	socket: WebSocket;
+}
+
+interface ServerListItem {
+	id: string;
+	name: string;
+	address: string;
+	socket: WebSocket;
+}
+
+interface MessageItem {
+	id: MessageID;
+	content: any;
+}
+
+interface ServerConnectContent {
+	name: string;
+	address: string;
+}
+
+const clients: Array<ClientListItem> = [];
+const servers: Array<ServerListItem> = [];
 
 console.clear();
 // console.log(servers.size, clients.size);
 
 WSS.on('connection', (ws, req) => {
 	ws.on('close', (data) => {
-		for (let [key, value] of servers) {
-			if (value.socket !== ws) continue;
+		if (servers.find((s) => s.socket === ws)) {
+			const server = servers.find((s) => s.socket === ws);
 
-			console.log(`Server disconnected: ${key}`);
-			servers.delete(key);
+			servers.splice(servers.indexOf(server), 1);
 
-			const serverlist = Array.from(servers, ([k, v]) => ({
-				id: k,
-				name: v.name,
-				address: v.address,
-			}));
-			clients.forEach((client) => {
-				if (client.socket.readyState === WebSocket.OPEN) {
-					client.socket.send(
-						JSON.stringify({
-							id: 'user-serverlist',
-							content: serverlist,
-						})
-					);
-				}
+			const u_list = clients.map(({ id }) => id);
+
+			clients.forEach(({ socket }) => {
+				if (socket.readyState !== WebSocket.OPEN) return;
+				socket.send(
+					JSON.stringify({
+						id: 'user-serverlist',
+						content: u_list,
+					})
+				);
 			});
-		}
-		for (let [key, value] of clients) {
-			if (value.socket !== ws) continue;
-			console.log(`User disconnected: ${key}`);
-			clients.delete(key);
+
+			console.info(`Server disconnected: ${server.id}`);
+		} else {
+			const client = clients.find((c) => c.socket === ws);
+
+			clients.splice(clients.indexOf(client), 1);
+			console.info(`User disconnected: ${client.name}`);
 		}
 	});
 
 	ws.on('message', (data: string) => {
-		const message = JSON.parse(data);
+		const { id, content }: MessageItem = JSON.parse(data);
 
-		switch (message.id) {
-			case 'user-connect':
-				let clientid = Math.random().toString().slice(2, 10);
-				while (clients.has(clientid))
-					clientid = Math.random().toString().slice(2, 10);
+		if (id === 'server-connect') {
+			const {
+				name,
+				address,
+			}: ServerConnectContent = content as ServerConnectContent;
+			// Generate random server ID (random 5 character string of numbers)
+			let s_id = Math.random().toString().slice(2, 7);
+			while (servers.some((s) => s.id === s_id))
+				s_id = Math.random().toString().slice(2, 7);
 
-				clients.set(clientid, {
-					name: message.content,
-					socket: ws,
-				});
+			// Create new ServerListItem based on that key and the incoming data
+			servers.push({
+				id: s_id,
+				name,
+				address,
+				socket: ws,
+			});
 
-				ws.send(
+			// Update all the clients with the new server
+			clients.forEach(({ socket }) => {
+				if (socket.readyState !== WebSocket.OPEN) return;
+				socket.send(
 					JSON.stringify({
-						id: 'user-connect-confirm',
-						content: clientid,
+						id: 'user-serverlist-update',
+						content: {
+							name,
+							id: s_id,
+						},
 					})
 				);
+			});
 
-				ws.send(
-					JSON.stringify({
-						id: 'user-serverlist',
-						content: Array.from(servers, ([key, value]) => ({
-							id: key,
-							name: value.name,
-							address: value.address,
-						})),
-					})
-				);
-
-				console.log(`User connected: ${clientid}`);
-				break;
-			case 'server-connect':
-				let serverid = Math.random().toString().slice(2, 7);
-				while (clients.has(serverid))
-					serverid = Math.random().toString().slice(2, 7);
-
-				servers.set(serverid, {
-					name: message.content.name,
-					address: message.content.address,
-					socket: ws,
-				});
-
-				ws.send(
-					JSON.stringify({
-						id: 'server-connect-confirm',
-						content: serverid,
-					})
-				);
-
-				const serverlist = Array.from(servers, ([key, value]) => ({
-					id: key,
-					name: value.name,
-					address: value.address,
-				}));
-				clients.forEach((client) => {
-					if (client.socket.readyState === WebSocket.OPEN) {
-						client.socket.send(
-							JSON.stringify({
-								id: 'user-serverlist',
-								content: serverlist,
-							})
-						);
-					}
-				});
-
-				console.log(`Server connected: ${serverid}`);
-				break;
-			default:
-				break;
+			console.info(`Server connected ${s_id}`);
 		}
+		// switch (message.id) {
+		// 	case 'user-connect':
+		// 		let clientid = Math.random().toString().slice(2, 10);
+		// 		while (clients.has(clientid))
+		// 			clientid = Math.random().toString().slice(2, 10);
+
+		// 		clients.set(clientid, {
+		// 			name: message.content,
+		// 			socket: ws,
+		// 		});
+
+		// 		ws.send(
+		// 			JSON.stringify({
+		// 				id: 'user-connect-confirm',
+		// 				content: clientid,
+		// 			})
+		// 		);
+
+		// 		ws.send(
+		// 			JSON.stringify({
+		// 				id: 'user-serverlist',
+		// 				content: Array.from(servers, ([key, value]) => ({
+		// 					id: key,
+		// 					name: value.name,
+		// 					address: value.address,
+		// 				})),
+		// 			})
+		// 		);
+
+		// 		console.log(`User connected: ${clientid}`);
+		// 		break;
+
+		// 	default:
+		// 		break;
+		// }
 
 		// console.clear();
 		// console.log(servers.size, clients.size);
