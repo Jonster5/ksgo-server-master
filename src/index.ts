252ee70d3d6bcb5207d0b965aca6403c9c9e0789
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2021-02-05 09:15:56 -0500

    package.json fix

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,38 @@ import WebSocket from 'ws';
 const port = process.env.PORT || 4000;
 const WSS = new WebSocket.Server({ port: parseInt(`${port}`) });
 
-type MessageID =
+declare type MessageID =
 	| 'server-connect'
 	| 'server-connect-confirm'
 	| 'user-connect'
 	| 'user-connect-confirm'
 	| 'user-serverlist-update';
 
-interface ClientListItem {
+declare interface ClientListItem {
 	id: string;
 	name: string;
 	socket: WebSocket;
 }
 
-interface ServerListItem {
+declare interface ServerListItem {
 	id: string;
 	name: string;
 	address: string;
 	socket: WebSocket;
 }
 
-interface MessageItem {
+declare interface MessageItem {
 	id: MessageID;
 	content: any;
 }
 
-interface ServerConnectContent {
+declare interface ServerConnectContent {
 	name: string;
 	address: string;
+}
+
+declare interface ClientConnectContent {
+	name: '';
 }
 
 const clients: Array<ClientListItem> = [];
@@ -63,7 +67,7 @@ WSS.on('connection', (ws, req) => {
 			const client = clients.find((c) => c.socket === ws);
 
 			clients.splice(clients.indexOf(client), 1);
-			console.info(`User disconnected: ${client.name}`);
+			console.info(`User disconnected: ${client.id}`);
 		}
 	});
 
@@ -88,21 +92,62 @@ WSS.on('connection', (ws, req) => {
 				socket: ws,
 			});
 
+			// Send confirmation to the server with it's new ID
+			ws.send(
+				JSON.stringify({
+					id: 'server-connect-confirm',
+					content: s_id,
+				} as MessageItem)
+			);
+
 			// Update all the clients with the new server
+			const si_list = servers.map(({ name, id }) => ({ name, id }));
+
 			clients.forEach(({ socket }) => {
 				if (socket.readyState !== WebSocket.OPEN) return;
 				socket.send(
 					JSON.stringify({
 						id: 'user-serverlist-update',
-						content: {
-							name,
-							id: s_id,
-						},
-					})
+						content: si_list,
+					} as MessageItem)
 				);
 			});
 
 			console.info(`Server connected ${s_id}`);
+		} else if (id === 'user-connect') {
+			const { name }: ClientConnectContent = content;
+
+			// Generate random client ID (random 8 character string of numbers)
+			let c_id = Math.random().toString().slice(2, 10);
+			while (clients.some(({ id }) => id === c_id))
+				c_id = Math.random().toString().slice(2, 10);
+
+			// Create new ClientListItem based on that key and the incoming data
+			clients.push({
+				id: c_id,
+				name,
+				socket: ws,
+			});
+
+			// Send confirmation to the client with it's new ID
+			ws.send(
+				JSON.stringify({
+					id: 'user-connect-confirm',
+					content: c_id,
+				} as MessageItem)
+			);
+
+			// Update the client serverlist
+			const si_list = servers.map(({ name, id }) => ({ name, id }));
+
+			ws.send(
+				JSON.stringify({
+					id: 'user-serverlist-update',
+					content: si_list,
+				} as MessageItem)
+			);
+
+			console.info(`User connected: ${c_id}`);
 		}
 		// switch (message.id) {
 		// 	case 'user-connect':
